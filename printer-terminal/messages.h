3dae5c7f9eb4c222841e3f329933b0c2407fbbf5
nathcampreguer <nathcampreguer@users.noreply.github.com>
2025-05-27 16:16:09 +0200

    Add support files

---
+++ printer-terminal/messages.h
@@ -0,0 +1,9 @@
+char *messages[] {
+  "Find someone who leaves their mug on top of the sink (instead of placing it in the dishwasher) and ask their reasoning to do so. Write it down on a post-it and place it on the cabinet (anonymously, please)",
+"Find someone who accumulates mugs in their office and ask why that happens. Write it down on a post-it and place it on the cabinet (anonymously, please)",
+"Who should be responsible for placing the mugs in the dishwasher? Write your answer on a post-it and stick it to the cabinet ",
+"How can/should individuals be responsible for maintaining a clean kitchen? Write your answer on a post-it and stick it to the cabinet ",
+"In the past days we saw that taking a few minutes to clean the kitchen can make a difference. Why do you think that doesn’t happen on a regular basis? Write your answer on a post-it and stick it to the cabinet ",
+"How could people self-organize a system to always bring back mugs? E.g., system to remind individuals or having someone (Employees? Cleaning staff?) go around and collect mugs once a week… Write an idea on a post-it and stick it to the cabinet "
+
+};
